69edf63f39c74e3f0ef1161ff4dc0bbcb6b7d2b3
Aymen-Lahdaoui <aymenlahdaoui70@gmail.com>
2026-07-14 13:49:52 +0100

    Implement update functionality for Parfum; add PutParfum method to handle updates and error responses

--- Controllers/ParfumsController.cs
+++ Controllers/ParfumsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OlfactiveParfum.Backend.Data;
 using OlfactiveParfum.Backend.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace OlfactiveParfum.Backend.Controllers
 {
@@ -29,6 +31,35 @@ namespace OlfactiveParfum.Backend.Controllers
             await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetParfums), new { id = parfum.Id }, parfum);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutParfum(int id, Parfum parfum)
+        {
+            if (id != parfum.Id)
+            {
+                return BadRequest("L'ID du produit ne correspond pas.");
+            }
+
+            var parfumExistant = await _context.Parfums.FindAsync(id);
+            if (parfumExistant == null)
+            {
+                return NotFound();
+            }
+
+            parfumExistant.Nom = parfum.Nom;
+            parfumExistant.Description = parfum.Description;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return StatusCode(500, "Erreur lors de la mise à jour.");
+            }
+
+            return NoContent();
         }
     }
 }
